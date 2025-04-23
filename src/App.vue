04ea9aeab952720b5d084e704986f6034d615489
ngoytrylyhuor <lyhourkhe123@gmail.com>
2025-04-23 13:38:36 +0700

    no message

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,15 @@
     <main class="container mx-auto px-4 py-8">
       <router-view />
     </main>
-    <Footer />
   </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue';
-import Footer from './components/Footer.vue';
-
 export default {
   name: 'App',
   components: {
-    NavBar,
-    Footer
+    NavBar
   }
 }
 </script>
