a21f2e8e2789321d00a070ba503320e49e31a4c6
ngoytrylyhuor <lyhourkhe123@gmail.com>
2025-04-24 23:57:57 +0700

    no message

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue';
 import router from './router';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://api.consatech-cambodia.com'; 
+// Set base URL based on current host
+axios.defaults.baseURL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' ? 'http://127.0.0.1:8000' : 'https://api.consatech-cambodia.com'; 
 
 const app = createApp(App);
 app.config.globalProperties.$http = axios; 
