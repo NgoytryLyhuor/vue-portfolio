1eb0c422a0e32e63a29d31f8a4078638fe1e2943
ngoytrylyhuor <lyhourkhe123@gmail.com>
2025-04-29 21:08:59 +0700

    no message

--- src/views/projects/WeatherApp.vue
+++ src/views/projects/WeatherApp.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="min-h-screen transition-colors duration-300">
-        <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-4 sm:py-8 mt-10">
+    <div class="min-h-screen py-12 px-4 sm:px-6 lg:px-8 transition-colors duration-300 from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800 text-gray-800 dark:text-gray-200">
+        <div class="max-w-3xl mx-auto">
             <!-- Header Section -->
             <div class="flex flex-col items-center text-center mb-5 mt-5">
                 <div>
