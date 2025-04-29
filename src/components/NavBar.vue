9564cd661bb796042ffe108daea6840381c49942
ngoytrylyhuor <lyhourkhe123@gmail.com>
2025-04-29 23:10:18 +0700

    no message

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -152,44 +152,54 @@
             leave-active-class="transition ease-in duration-150" leave-from-class="transform opacity-100 translate-y-0"
             leave-to-class="transform opacity-0 -translate-y-2">
             <div v-if="isMobileMenuOpen"
-                class="md:hidden py-2 px-4 space-y-2 mt-4 bg-white/95 dark:bg-gray-900/80 backdrop-blur-sm border-t dark:border-gray-700">
-                <router-link v-for="link in standardNavLinks" :key="link.path" :to="link.path"
-                    class="block hover:text-green-500 dark:hover:text-green-400 py-3 px-2 transition-colors duration-200 rounded-md"
-                    :class="{ 'text-green-500 dark:text-green-400 bg-gray-100 dark:bg-gray-800': $route.path === link.path }"
-                    @click="isMobileMenuOpen = false" :aria-label="link.label">
-                    {{ link.label }}
-                </router-link>
+                class="md:hidden absolute left-0 right-0 top-full bg-white/95 dark:bg-gray-900/95 backdrop-blur-sm border-t dark:border-gray-700 shadow-lg">
+                <div class="container mx-auto px-4 py-3 space-y-1">
+                    <router-link v-for="link in standardNavLinks" :key="link.path" :to="link.path"
+                        class="block py-3 px-4 hover:text-green-500 dark:hover:text-green-400 transition-colors duration-200 rounded-lg"
+                        :class="{
+                            'text-green-500 dark:text-green-400 bg-gray-100 dark:bg-gray-800 font-medium': $route.path === link.path,
+                            'text-gray-700 dark:text-gray-300': $route.path !== link.path
+                        }" @click="isMobileMenuOpen = false" :aria-label="link.label">
+                        {{ link.label }}
+                    </router-link>
 
-                <!-- Other Features section in mobile menu -->
-                <div class="py-2 px-2">
-                    <button @click="toggleMobileFeatures"
-                        class="flex items-center justify-between w-full py-3 px-2 hover:text-green-500 dark:hover:text-green-400 transition-colors duration-200 rounded-md"
-                        :class="{ 'text-green-500 dark:text-green-400': isFeatureActive }">
-                        <span>Other Features</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 ml-1" viewBox="0 0 20 20"
-                            fill="currentColor" :class="{ 'transform rotate-180': isMobileFeaturesOpen }">
-                            <path fill-rule="evenodd"
-                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                clip-rule="evenodd" />
-                        </svg>
-                    </button>
+                    <!-- Other Features section in mobile menu -->
+                    <div class="py-1">
+                        <button @click="toggleMobileFeatures"
+                            class="flex items-center justify-between w-full py-3 px-4 hover:text-green-500 dark:hover:text-green-400 transition-colors duration-200 rounded-lg"
+                            :class="{
+                                'text-green-500 dark:text-green-400 bg-gray-100 dark:bg-gray-800 font-medium': isFeatureActive,
+                                'text-gray-700 dark:text-gray-300': !isFeatureActive
+                            }">
+                            <span>Other Features</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-1" viewBox="0 0 20 20"
+                                fill="currentColor" :class="{ 'transform rotate-180': isMobileFeaturesOpen }">
+                                <path fill-rule="evenodd"
+                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </button>
 
-                    <transition enter-active-class="transition ease-out duration-200"
-                        enter-from-class="transform opacity-0 -translate-y-2"
-                        enter-to-class="transform opacity-100 translate-y-0"
-                        leave-active-class="transition ease-in duration-150"
-                        leave-from-class="transform opacity-100 translate-y-0"
-                        leave-to-class="transform opacity-0 -translate-y-2">
-                        <div v-if="isMobileFeaturesOpen" class="pl-4 space-y-2 mt-2">
-                            <router-link v-for="feature in featureLinks" :key="feature.path" :to="feature.path"
-                                class="flex items-center py-3 px-2 hover:text-green-500 dark:hover:text-green-400 transition-colors duration-200 rounded-md"
-                                :class="{ 'text-green-500 dark:text-green-400 bg-gray-100 dark:bg-gray-800': $route.path === feature.path }"
-                                @click="isMobileMenuOpen = false">
-                                <component :is="feature.icon" class="h-5 w-5 mr-3" />
-                                {{ feature.label }}
-                            </router-link>
-                        </div>
-                    </transition>
+                        <transition enter-active-class="transition ease-out duration-200"
+                            enter-from-class="transform opacity-0 -translate-y-2"
+                            enter-to-class="transform opacity-100 translate-y-0"
+                            leave-active-class="transition ease-in duration-150"
+                            leave-from-class="transform opacity-100 translate-y-0"
+                            leave-to-class="transform opacity-0 -translate-y-2">
+                            <div v-if="isMobileFeaturesOpen"
+                                class="pl-4 space-y-1 mt-1 border-l-2 border-gray-200 dark:border-gray-700 ml-4">
+                                <router-link v-for="feature in featureLinks" :key="feature.path" :to="feature.path"
+                                    class="flex items-center py-3 px-4 hover:text-green-500 dark:hover:text-green-400 transition-colors duration-200 rounded-lg"
+                                    :class="{
+                                        'text-green-500 dark:text-green-400 bg-gray-100 dark:bg-gray-800 font-medium': $route.path === feature.path,
+                                        'text-gray-600 dark:text-gray-400': $route.path !== feature.path
+                                    }" @click="isMobileMenuOpen = false">
+                                    <component :is="feature.icon" class="h-5 w-5 mr-3" />
+                                    {{ feature.label }}
+                                </router-link>
+                            </div>
+                        </transition>
+                    </div>
                 </div>
             </div>
         </transition>
